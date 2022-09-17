e00c7123ea1a2fc70a7bea4901f0fc1b14dd5fb8
Lamaglama39 <lamaglama621@gmail.com>
2022-09-17 15:41:28 +0900

    update choice_mac menu
    Add main, side and drink to arguments
    Split list into mains, sides and drinks

--- choice_mac/choice_mac.py
+++ choice_mac/choice_mac.py
@@ -1,17 +1,39 @@
 import random
 import menu
 import sys
-eats = int(sys.argv[1])
+mains = int(sys.argv[1])
+sides = int(sys.argv[2])
+drinks = int(sys.argv[3])
 
-def choice_mac(eats):
+def choice_mac(mains, sides, drinks):
     """今日食べるべきマックを選びます。
        
        いつもは頼まないメニューが食べたくなったら使ってね。
     """
+    #メインメニュー選択
+    print("----------------------------------")
+    print("メインはこちら。")
+    for main in range(mains):
+        choice = random.choice(menu.main_list)
+        main_number = main + 1
+        print("main" + str(main_number) + ":  " + choice)
+    print("----------------------------------")
     
-    for eat in range(1, eats):
-        choice = random.choice(menu.menu_list)
-        print(str(eat) + ":" + choice + "!!!")
+    #サイドメニュー選択
+    print("サイドはこちら。")
+    for side in range(sides):
+        choice = random.choice(menu.side_list)
+        side_number = side + 1
+        print("side" + str(side_number) + ":  " + choice)
+    print("----------------------------------")
+    
+    #ドリンクメニュー選択
+    print("ドリンクはこちら。")
+    for drink in range(drinks):
+        choice = random.choice(menu.drink_list)
+        drink_number = drink + 1
+        print("drink" + str(drink_number) + ":  " + choice)
+    print("----------------------------------")
 
 if __name__=='__main__':
-    choice_mac(eats)
+    choice_mac(mains, sides, drinks)
