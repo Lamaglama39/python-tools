8e7c52fcb08fb971dd7e5fc7cd8e400e56c0822e
Lamaglama39 <lamaglama621@gmail.com>
2022-09-19 21:55:48 +0900

    update choice_mac
    Add partition to arguments
    Change text output to f-strings

--- choice_mac/choice_mac.py
+++ choice_mac/choice_mac.py
@@ -4,6 +4,7 @@ import sys
 mains = int(sys.argv[1])
 sides = int(sys.argv[2])
 drinks = int(sys.argv[3])
+partition = "-" * 35
 
 def choice_mac(mains, sides, drinks):
     """今日食べるべきマックを選びます。
@@ -11,29 +12,29 @@ def choice_mac(mains, sides, drinks):
        いつもは頼まないメニューが食べたくなったら使ってね。
     """
     #メインメニュー選択
-    print("----------------------------------")
+    print(partition)
     print("メインはこちら。")
     for main in range(mains):
         choice = random.choice(menu.main_list)
-        main_number = main + 1
-        print("main" + str(main_number) + ":  " + choice)
-    print("----------------------------------")
+        main_number = str(main + 1)
+        print(f'main{main_number}: {choice}')
+    print(partition)
     
     #サイドメニュー選択
     print("サイドはこちら。")
     for side in range(sides):
         choice = random.choice(menu.side_list)
-        side_number = side + 1
-        print("side" + str(side_number) + ":  " + choice)
-    print("----------------------------------")
+        side_number = str(side + 1)
+        print(f'side{side_number}: {choice}')
+    print(partition)
     
     #ドリンクメニュー選択
     print("ドリンクはこちら。")
     for drink in range(drinks):
         choice = random.choice(menu.drink_list)
-        drink_number = drink + 1
-        print("drink" + str(drink_number) + ":  " + choice)
-    print("----------------------------------")
+        drink_number = str(drink + 1)
+        print(f'drink{drink_number}: {choice}')
+    print(partition)
 
 if __name__=='__main__':
     choice_mac(mains, sides, drinks)
