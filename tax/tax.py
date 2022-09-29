8f1cd73f161907e11c19987a9565abdf7e018a9f
Namako <kanikama775@gmail.com>
2022-09-29 23:59:41 +0900

    add currency_exchange
    Added currency conversion function

--- tax/tax.py
+++ tax/tax.py
@@ -1,11 +1,47 @@
 import decimal
+def tax_calculation():
+    price = int(input("price: "))
+    tax_late = decimal.Decimal(input("tax_late: "))
 
-price = int(input("price: "))
-tax_late = decimal.Decimal(input("tax_late: "))
+    def tax(price, tax_late=1.08):
+        print("print:",price,"tac_late:",tax_late)
+        tax_price = decimal.Decimal(price * tax_late)
+        print("tax_price:", "{:,}".format(tax_price))
+    tax(price, tax_late)
+# tax_calculation()
 
-def tax(price, tax_late=1.08):
-    print("print:",price,"tac_late:",tax_late)
-    tax_price = decimal.Decimal(price * tax_late)
-    print("tax_price:", "{:,}".format(tax_price))
+from currency_converter import CurrencyConverter
+from datetime import date
+import math
 
-tax(price, tax_late)
+def currency_exchange():
+    c = CurrencyConverter()
+
+    before  = input("為替元の国を入力してください(Default:JPY): ")
+    money = input("金額を入力してください(Default:1): ")
+    after = input("為替先の国を入力してください(Default:USD): ")
+
+    if  money.isdecimal():
+        pass
+    else :
+        money = 1
+
+    if  before.isupper():
+        pass
+    else :
+        before = "JPY"
+
+    if  after.isupper():
+        pass
+    else :
+        after = "USD"
+
+    choice_date = date(2022, 8, 26)
+
+    def exchange(money, before, after, choice_date):
+        answer = (c.convert(money, before, after,choice_date))
+        answer3 = math.floor(answer * 10 ** 2) / (10 ** 2)
+        print("tax_price:", ("{:,}".format(answer3)))
+    exchange(money, before, after, choice_date)
+
+# currency_exchange()
