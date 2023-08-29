cad96031f1d1767d08ff4338a4f25c69235185b8
Lamaglama39 <lamaglama621@gmail.com>
2023-08-29 22:53:32 +0900

    :sparkles: add get server info function

---
+++ info-self.py
@@ -0,0 +1,15 @@
+import socket
+import requests
+
+
+def info():
+    host_name = socket.gethostname()
+    local_ip = socket.gethostbyname(host_name)
+    global_ip = requests.get('https://ifconfig.me').text
+    print(f'HostName: {host_name}')
+    print(f'Local IP: {local_ip}')
+    print(f'Global IP: {global_ip}')
+
+
+if __name__ == "__main__":
+    info()
