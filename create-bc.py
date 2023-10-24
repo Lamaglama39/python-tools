08e9be6527496281dcd29a6dd82261142858870e
Lamaglama39 <lamaglama621@gmail.com>
2023-10-24 23:26:28 +0900

    :construction: add shebang

--- create-bc.py
+++ create-bc.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import barcode
 from barcode.writer import ImageWriter
 import argparse
--- get-page-screenshot.py
+++ get-page-screenshot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import base64
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
